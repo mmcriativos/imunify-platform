2f703220eac17a8d00b7ef7367b9afebf74d9190
Imunify <imunify@imunify.com.br>
2025-11-25 16:45:00 -0300

    Corrige configuração de conexão no comando tenant:reset

--- app/Console/Commands/ResetTenantDatabase.php
+++ app/Console/Commands/ResetTenantDatabase.php
@@ -52,13 +52,16 @@ class ResetTenantDatabase extends Command
             // Conectar ao database do tenant
             config(['database.connections.tenant_clean' => [
                 'driver' => 'mysql',
-                'host' => env('DB_HOST'),
-                'port' => env('DB_PORT'),
+                'host' => env('DB_HOST', '127.0.0.1'),
+                'port' => env('DB_PORT', '3306'),
                 'database' => $databaseName,
-                'username' => env('DB_USERNAME'),
-                'password' => env('DB_PASSWORD'),
+                'username' => env('DB_USERNAME', 'forge'),
+                'password' => env('DB_PASSWORD', ''),
                 'charset' => 'utf8mb4',
                 'collation' => 'utf8mb4_unicode_ci',
+                'prefix' => '',
+                'strict' => true,
+                'engine' => null,
             ]]);
 
             DB::purge('tenant_clean');
