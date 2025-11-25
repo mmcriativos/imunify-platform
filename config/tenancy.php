f69145e966f83cf5b4ae5b688494333ee8795b4d
Imunify <imunify@imunify.com.br>
2025-11-25 16:32:40 -0300

    Desabilita CacheTenancyBootstrapper para evitar erro de cache tagging

--- config/tenancy.php
+++ config/tenancy.php
@@ -32,7 +32,7 @@ return [
      */
     'bootstrappers' => [
         Stancl\Tenancy\Bootstrappers\DatabaseTenancyBootstrapper::class,
-        Stancl\Tenancy\Bootstrappers\CacheTenancyBootstrapper::class,
+        // Stancl\Tenancy\Bootstrappers\CacheTenancyBootstrapper::class, // Desabilitado: requer cache com tags (Redis/Memcached)
         Stancl\Tenancy\Bootstrappers\FilesystemTenancyBootstrapper::class,
         Stancl\Tenancy\Bootstrappers\QueueTenancyBootstrapper::class,
         // Stancl\Tenancy\Bootstrappers\RedisTenancyBootstrapper::class, // Note: phpredis is needed
